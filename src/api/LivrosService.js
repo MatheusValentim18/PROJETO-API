2a67367984c6ae1e643772717082fdc62f065a62
MatheusValentim18 <154943790+MatheusValentim18@users.noreply.github.com>
2024-04-19 09:03:38 -0300

    Update LivrosService.js

--- src/api/LivrosService.js
+++ src/api/LivrosService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://backend-wa45maw6l-matheus-valentims-projects.vercel.app";
+const BASE_URL = "http://localhost:3000";
 
 export const LivrosService = {
   getLivros: () => axios.get(`${BASE_URL}/livros`),
